5ab0fc22362cd3cefb43a1566c02db696af27563
ChungChiuHung <nsdrum@hotmail.com>
2024-03-10 22:56:31 +0800

    extract servo params as a class

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from time import sleep
 from flask import Flask, render_template, request
 
 from checksum import CRC16CCITT
+from servoparams import ServoParams
 
 # RSE TX/RX Control Pin
 RS485_ENABLE_PIN = 4
@@ -55,139 +56,6 @@ GPIO.output(ledRed, GPIO.LOW)
 GPIO.output(ledYlw, GPIO.LOW)
 GPIO.output(ledGrn, GPIO.LOW)
 
-# Data Format
-# A     B     C  D  E         F
-# 24 01 00 04 00 04 ****...** ****
-# 26 01 00 66 01 20 
-# E (2~29 byte)
-# 參數(No.) 寫入值 狀態編號 解鎖代碼
-# F 錯誤檢出代碼
-# SET_STATE_VALUE_WITHMASK_4
-# E I/O Section
-# 狀態編號            2 bytes
-# 設定的狀態值        4 bytes
-# 指定的位元/遮罩Mask 4 bytes
-#    - 0 不修改
-#    - 1 修改
-# Example:
-#   原始狀態  : 0F 00 00 00
-#   設定值    : 60 00 00 01
-#   遮罩      : 00 00 00 01
-#   寫入後狀態: 0F 00 00 01
-
-# Response: E I/O Section
-# 處理結果 2 bytes
-#    - 0x00 正常
-#    - 0x01 異常
-# 被寫入的值 4 bytes
-
-SET_PARAM_2 = [0x26,0x01,
-               0x00,0x07, # 數據代碼
-               0x00,0x09, # 參數群組 
-               0x00,0x01, # 設定值
-               0x9B,0x98]
-# 0x00, 0x07 Write 2 bytes
-# 0x00, 0x08 Write 4 bytes
-
-
-SERVO_ON = [0x2C,0x01,
-            0x00,0x66,
-            0x01,0x20,
-            0x00,0x00,0x00,0x01,
-            0x00,0x00,0x00,0x01,
-            0x0A,0x9A]
-
-SERVO_OFF = [0x2C,0x01,
-             0x00,0x66,
-             0x01,0x20,
-             0x00,0x00,0x00,0x00,
-             0x00,0x00,0x00,0x01,
-             0xA0,0xCB]
-            
-GO_ZERO_cmd1 = [0x2C,0x01,
-                0x00,0x66,
-                0x01,0x20,
-                0x00,0x08,0x00,0x00,
-                0x00,0x08,0x00,0x00,
-                0x8A,0xE6]
-         
-GO_ZERO_cmd2 = [0x2C,0x01,
-                0x00,0x66,
-                0x01,0x20,
-                0x00,0x00,0x00,0x00,
-                0x00,0x08,0x00,0x00,
-                0x19,0x4B]
-
-SET_ZERO_POINT = [0x2C,0x01,
-                  0x00,0x66,
-                  0x01,0x20,
-                  '*','*',0x00,0x00,
-                  0x3C,0x00,0x00,0x00,
-                  '*','*']
-
-Motor_Activate_1 = [0x2C,0x01,
-                    0x00,0x66,
-                    0x01,0x20,
-                    0x01,0x00,0x00,0x00,
-                    0x01,0x00,0x00,0x00,
-                    0x81,0x8D]
-
-Motor_Activate_2 = [0x2C,0x01,
-                    0x00,0x66,
-                    0x01,0x20,
-                    0x00,0x00,0x00,0x00,
-                    0x01,0x00,0x00,0x00,
-                    0xC6,0x5E]
-
-Motor_Pause_1 = [0x2C,0x01,
-                 0x00,0x66,
-                 0x01,0x20,
-                 0x00,0x20,0x00,0x00,
-                 0x01,0x20,0x00,0x00,
-                 0x58,0xDA]
-
-Motor_Pause_2 = [0x2C,0x01,
-                 0x00,0x66,
-                 0x01,0x20,
-                 0x00,0x00,0x00,0x00,
-                 0x01,0x20,0x00,0x00,
-                 0x36,0x2C]
-
-Motor_Restart_1 = [0x2C,0x01,
-                   0x00,0x66,
-                   0x01,0x20,
-                   0x00,0x20,0x00,0x00,
-                   0x01,0x20,0x00,0x00,
-                   0x58,0xDA]
-
-Motor_Restart_2 = [0x2C,0x01,
-                   0x00,0x66,
-                   0x01,0x20,
-                   0x00,0x00,0x00,0x00,
-                   0x01,0x20,0x00,0x00,
-                   0x36,0x2C]
-
-GET_STATE_VALUE_4 = [0x24,0x01,
-                     0x00,0x11,
-                     0x01,0x28,
-                     0x75,0xE0]
-# response
-# 26 01 80 11 (* *) (* *) (* *) (* *) (* *)(* *)
-#
-
-# Example usage:
-crc_calculator = CRC16CCITT()
-test_data_list = [0x2C,0x01,
-                  0x00,0x66,
-                  0x01,0x20,
-                  0x00,0x00,0x00,0x01,
-                  0x00,0x00,0x00,0x01]
-result_data_with_crc_list = crc_calculator.append_crc_as_list(test_data_list)
-
-print("Original data as list:", test_data_list)
-print("Data with CRC-16-CCITT appended as list:", result_data_with_crc_list)
-
-
 @app.route("/")
 def index():
 	# Read Sensors Status
@@ -233,15 +101,15 @@ def action(deviceName, action):
             else: 
                   GPIO.output(actuator, GPIO.LOW)
       
-      if action == "servoOn":
+      elif action == "servoOn":
             print("SERVO ON")            
             print("SET PARAM 2!")
 
-            bytes_object = bytes(SET_PARAM_2)
+            bytes_object = bytes(ServoParams.SET_PARAM_2)
             RS485_send = str(bytes_object)
 
-            ser_port.write(SET_PARAM_2)
-            sleep(0.05)
+            ser_port.write(ServoParams.SET_PARAM_2)
+            sleep(0.1)
             print("Response: ")
             result_1 = ser_port.inWaiting()
             result_2 = ser_port.read(ser_port.inWaiting())
@@ -251,9 +119,9 @@ def action(deviceName, action):
             RS485_read = str(result_2)
 
             print("SERVO ON")
-            ser_port.write(SERVO_ON)
+            ser_port.write(ServoParams.SERVO_ON)
             # ser_port.write(result_data_with_crc_list)
-            sleep(0.05)
+            sleep(0.1)
 
             print("Response:")
             result_1 = ser_port.inWaiting()
@@ -261,12 +129,12 @@ def action(deviceName, action):
             print(result_1)
             print(result_2)
 
-      if action == "servoOff":
+      elif action == "servoOff":
             print("SERVO OFF")
-            bytes_object = bytes(SERVO_OFF)
+            bytes_object = bytes(ServoParams.SERVO_OFF)
             RS485_send = str(bytes_object)
-            ser_port.write(SERVO_OFF)
-            sleep(0.05)
+            ser_port.write(ServoParams.SERVO_OFF)
+            sleep(0.1)
             print("Response:")
             result_1 = ser_port.inWaiting()
             result_2 = ser_port.read(ser_port.inWaiting())
@@ -274,12 +142,12 @@ def action(deviceName, action):
             print(result_2)
             RS485_read = str(result_2)
       
-      if action == "getMsg":
+      elif action == "getMsg":
             print("GET VALUE")
-            bytes_object = bytes(GET_STATE_VALUE_4)
+            bytes_object = bytes(ServoParams.GET_STATE_VALUE_4)
             RS485_send = str(bytes_object)
-            ser_port.write(GET_STATE_VALUE_4)
-            sleep(0.05)
+            ser_port.write(ServoParams.GET_STATE_VALUE_4)
+            sleep(0.1)
             print("Response:")
             result_1 = ser_port.inWaiting()
             result_2 = ser_port.read(ser_port.inWaiting())
