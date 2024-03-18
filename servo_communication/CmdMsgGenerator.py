6c09de96fa9d3d37d37635a138e2fc7f99bcd913
JoJoHung <52248840+ChungChiuHung@users.noreply.github.com>
2024-03-18 14:50:17 +0800

    replace match to if else

--- servo_communication/CmdMsgGenerator.py
+++ servo_communication/CmdMsgGenerator.py
@@ -20,6 +20,7 @@ class MessageGenerator:
         return ' '.join(format(byte, '02X') for byte in command_with_crc) if return_as_str else command_with_crc
 
     def get_command(self, command_code_enum, return_as_str=True, **kwargs):
+        '''
         match command_code_enum:
             case CmdCode.NOP | CmdCode.UNLOCK_PARAM_ALL:
                 parameter_code = []
@@ -64,3 +65,48 @@ class MessageGenerator:
                 raise NotImplementedError(f"Command {command_code_enum.name} is not implemented.")
 
         return self.generate_command(command_code_enum.value, parameter_code, return_as_str)
+        '''
+        if command_code_enum in [CmdCode.NOP, CmdCode.UNLOCK_PARAM_ALL]:
+            parameter_code = []
+        elif command_code_enum == CmdCode.SET_PARAM_2:
+            param_group = kwargs.get('param_group', [])
+            write_value = kwargs.get('write_value', [])
+            if not (len(param_group) == 2 and len(write_value) == 2):
+                raise ValueError("Incorrect parameter length for SET_PARAM_2.")
+            parameter_code = param_group + write_value
+        elif command_code_enum == CmdCode.SET_PARAM_4:
+            param_group = kwargs.get('param_group', [])
+            write_value = kwargs.get('write_value', [])
+            if not (len(param_group) == 2 and len(write_value) == 4):
+                raise ValueError("Incorrect parameter length for SET_PARAM_4.")
+            parameter_code = param_group + write_value
+        elif command_code_enum in [CmdCode.SAVE_PARAM_ALL, CmdCode.GET_PARAM_2, CmdCode.GET_PARAM_4, CmdCode.GET_STATE_VALUE_2, CmdCode.GET_STATE_VALUE_4, CmdCode.CLEAR_EA05_DATA]:
+            parameter = kwargs.get('parameter', [])
+            if not (len(parameter) == 2):
+                raise ValueError(f"Incorrect parameter length for {command_code_enum.name}.")
+            parameter_code = parameter
+        elif command_code_enum == CmdCode.READ_EA05_DATA:
+            alarm_info = kwargs.get('alarm_info', [])
+            fixed = kwargs.get('fixed', [])
+            if not (len(alarm_info) == 2 and len(fixed) == 2):
+                raise ValueError("Incorrect parameter length for READ_EA05_DATA.")
+            parameter_code = alarm_info + fixed
+        elif command_code_enum == CmdCode.READ_EA05_DATA_EX:
+            alarm = kwargs.get('alarm', [])
+            single_turn_data = kwargs.get('single_turn_data', [])
+            multi_turn_data = kwargs.get('multi_turn_data', [])
+            if not (len(alarm) == 2 and len(single_turn_data) == 2 and len(multi_turn_data) == 2):
+                raise ValueError("Incorrect parameter length for READ_EA05_DATA_EX.")
+            parameter_code = alarm + single_turn_data + multi_turn_data
+        elif command_code_enum == CmdCode.SET_STATE_VALUE_WITHMASK_4:
+            status_number = kwargs.get('status_number', [])
+            status_value = kwargs.get('status_value', [])
+            mask = kwargs.get('mask', [])
+            if not (len(status_number) == 2 and len(status_value) == 4 and len(mask) == 4):
+                raise ValueError("Incorrect parameter length for SET_STATE_VALUE_WITHMASK_4.")
+            parameter_code = status_number + status_value + mask
+        else:
+            raise NotImplementedError(f"Command {command_code_enum.name} is not implemented.")
+
+        return parameter_code
+        
