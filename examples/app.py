0be14896db1122734d9eacd8c23f2d40cd5be558
JoJoHung <52248840+ChungChiuHung@users.noreply.github.com>
2024-03-24 23:46:19 +0000

    update

--- examples/app.py
+++ examples/app.py
@@ -26,8 +26,8 @@ if not serial_port_handler.open_serial():
 serial_comm = SerialCommunication(port_handler = serial_port_handler, command_timeout=2)
 
 # Initailize global variables for RS485 messages
-RS485_send = ''
-RS485_read = ''
+RS485_send = "00 00 FF FF"
+RS485_read = "FF FF 00 00"
 ser_port = serial_port_handler
 
 SERVO_ON = False
@@ -61,14 +61,33 @@ error_code = 0
 def home():
       return render_template('home.html')
 
+@app.route('/index')
+def index():
+      templateData = {
+        'title': 'Servo Control Panel',
+        'RS485_read': RS485_send,
+        'RS485_send': RS485_read,
+        'SERVO_ON' : SERVO_ON,
+        'SERVO_OFF' : SERVO_OFF,
+        'GET_MSG' : GET_MSG,
+        'GET_IO_OUTPUT' : GET_IO_OUTPUT,
+        'SET_POINT_1' : SET_POINT_1,
+        'SET_POINT_2' : SET_POINT_2,
+        'SET_POINT_HOME' : SET_POINT_HOME,
+        'MOTION_START' : MOTION_START,
+        'MOTION_PAUSE' : MOTION_PAUSE,
+      }
+      return render_template('index.html', **templateData) 
+
 @app.route('/action', methods=['POST'])
-def handle_action():
-      action_type = request.get_json('action')
-      response = {"status":"success","action":action_type}
-      print(f"Received action: {action_type}")
+
+def handle_action(action):
+      global SERVO_ON, SERVO_OFF, GET_MSG, GET_IO_OUTPUT, SET_POINT_1, SET_POINT_2, SET_POINT_HOME, MOTION_START, MOTION_PAUSE
+      response = {"status":"success","action":action}
+      print(f"Received action: {action}")
       # Perform the raspi action here based on action type
 
-      if action_type == "servoOn":
+      if action == "servoOn":
             # SET_PARAM_2 command        
             set_param_2_command = ServoParams.SET_PARAM_2      
             serial_comm.send_command_and_wait_for_response(set_param_2_command,
@@ -86,7 +105,7 @@ def handle_action():
                                                            "SERVO_ON")
             response['message']="Servo turned on successfully."
 
-      elif action_type == "servoOff":
+      elif action == "servoOff":
             cmd_code = CmdCode.SET_STATE_VALUE_WITHMASK_4.value
             parameter_data = setter.set_bit_status(BitMap.SVON, 0)
             servo_off_command = cmd_generator.generate_message(
@@ -99,7 +118,7 @@ def handle_action():
             
             response['message'] = "Servo turned off successfully."
       
-      elif action_type == "getMsg":
+      elif action == "getMsg":
             get_state_value_command = ServoParams.GET_STATE_VALUE_4
             result, response_received = serial_comm.send_command_and_wait_for_response(get_state_value_command,
                                                            "GET_STATE_VALUE_4")
@@ -114,14 +133,14 @@ def handle_action():
 
             response['message'] = "Get Servo IO Input Status Value."
 
-      elif action_type == "getIOOutput":
+      elif action == "getIOOutput":
             # RS485_read = fetcher.get_output_io_status()
             # print(RS485_read)
             result, response_received = serial_comm.get_output_io_status()
 
             response['message'] = "Get Servo IO Output Status Value."
 
-      elif action_type == "setPoint_1":
+      elif action == "setPoint_1":
             cmd_code = CmdCode.SET_STATE_VALUE_WITHMASK_4.value
             parameter_data = setter.set_bit_status(BitMap.SEL_NO, 5)
             point_sel_command = cmd_generator.generate_message(
@@ -133,7 +152,7 @@ def handle_action():
 
             response['message'] = "Set the Postion in Point 1."
 
-      elif action_type == "setPoint_2":
+      elif action == "setPoint_2":
             cmd_code = CmdCode.SET_STATE_VALUE_WITHMASK_4.value
             parameter_data = setter.set_bit_status(BitMap.SEL_NO, 6)
             point_sel_command = cmd_generator.generate_message(
@@ -146,7 +165,7 @@ def handle_action():
 
             response['message'] = "Set the Postion in Point 2."
 
-      elif action_type == "Home":
+      elif action == "Home":
             cmd_code = CmdCode.SET_STATE_VALUE_WITHMASK_4.value
             parameter_data = setter.set_bit_status(BitMap.SEL_NO, 1)
             point_sel_command = cmd_generator.generate_message(
@@ -159,7 +178,7 @@ def handle_action():
             
             response['message'] = "Set the Postion in HOME."
             
-      elif action_type == "motionStart":
+      elif action == "motionStart":
             print("START")
             cmd_code = CmdCode.SET_STATE_VALUE_WITHMASK_4.value
             parameter_data = setter.set_bit_status(BitMap.START1, 0)
@@ -183,7 +202,7 @@ def handle_action():
             response['message'] = "Motion Start."
 
 
-      elif action_type == "motionPause":
+      elif action == "motionPause":
             temp = pause_toggle_bit
             print("PAUSE: ",temp)
             cmd_code = CmdCode.SET_STATE_VALUE_WITHMASK_4.value
@@ -203,24 +222,6 @@ def handle_action():
             response['error'] = "Action not recognized."
 
       return jsonify(response)
-
-@app.route('/index')
-def index():
-      templateData = {
-        'title': 'Servo Control Panel',
-        'RS485_read': serial_comm.last_send_message,
-        'RS485_send': serial_comm.last_received_message,
-        'SERVO_ON' : SERVO_ON,
-        'SERVO_OFF' : SERVO_OFF,
-        'GET_MSG' : GET_MSG,
-        'GET_IO_OUTPUT' : GET_IO_OUTPUT,
-        'SET_POINT_1' : SET_POINT_1,
-        'SET_POINT_2' : SET_POINT_2,
-        'SET_POINT_HOME' : SET_POINT_HOME,
-        'MOTION_START' : MOTION_START,
-        'MOTION_PAUSE' : MOTION_PAUSE,
-      }
-      return render_template('index.html', **templateData) 
 
 if __name__ == "__main__":
    gpio_utils.initialize_gpio()
