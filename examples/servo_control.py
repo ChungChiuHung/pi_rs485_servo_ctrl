2d1d7543104dc8d912d63feb011293b71f0a1beb
JoJoHung <52248840+ChungChiuHung@users.noreply.github.com>
2024-03-26 15:33:00 +0800

    update

--- examples/servo_control.py
+++ examples/servo_control.py
@@ -7,18 +7,17 @@ from servo_params import ServoParams
 from set_servo_io_status import SetServoIOStatus
 from status_bit_mapping import BitMap, BitMapOutput
 
-class ServoCntroller:
+class ServoController:
     def __init__(self, serial_port):
         self.serial_port = serial_port
         self.cal_command_time_delay = CmdDelayTime(self.serial_port.baud_rate)
-        self.last_send_message = b''
-        self.last_received_message = b''
+        self._last_send_message = b''
+        self._last_received_message = b''
+        init() # Initialize colorama
 
-    @staticmethod
     def delay_ms(self, milliseconds):
       time.sleep(milliseconds / 1000.0)
 
-    @staticmethod
     def print_byte_array_as_spaced_hex(self,byte_array, data_name):
         hex_string = ' '.join(f"{byte:02X}" for byte in byte_array)
         print(f"{data_name}: {hex_string}")
@@ -38,7 +37,7 @@ class ServoCntroller:
             print("Serial port is not open.")
             return None
         
-        self.last_send_message = command
+        self._last_send_message = command
         print(f"Sending {command_description}:")
         self.print_byte_array_as_spaced_hex(command, command_description)
 
@@ -56,7 +55,7 @@ class ServoCntroller:
                 elapsed_time = time.time() -start_time
                 self.create_progress_bar(elapsed_time/self.command_timeout)
 
-            self.last_received_message = response
+            self._last_received_message = response
 
             if response:
                 print(f"\n{command_description} response received")
@@ -72,6 +71,7 @@ class ServoCntroller:
         
         return response
      
+    @staticmethod
     def create_progress_bar(progress, bar_length=30):
         filled_length = int(round(bar_length * progress))
         bar = Fore.GREEN + '█' * filled_length + Fore.RED + '█' * (bar_length - filled_length)
