cac8a5a24acf45e6001dd60a834934819834c241
JoJoHung <52248840+ChungChiuHung@users.noreply.github.com>
2024-03-26 17:29:43 +0800

    update

--- examples/app.py
+++ examples/app.py
@@ -100,18 +100,30 @@ def handle_action():
 
       if action == "servoOn":
             # SET_PARAM_2 command        
-            command_code = CmdCode.NOP
+            command_code = CmdCode.SET_PARM_2
             command_format = SerialPotocolHandler()
-            nop_command = command_format.construct_packet(1,command_code, b'', is_response=False)
-            print(f"{command_code.name} Command: ", nop_command.hex())
+            set_param_2_command = command_format.construct_packet(1,command_code, b'\x00\x09\x00\x01', is_response=False)
+            print(f"{command_code.name} Command: ", set_param_2_command.hex())
 
-            servo_ctrller.send_command_and_wait_for_response(nop_command, f"{command_code.name}", 0.05)
+            servo_ctrller.send_command_and_wait_for_response(set_param_2_command, f"{command_code.name}", 0.05)
             # SERVO_ON Command
-            
+            command_code = CmdCode.SET_STATE_VALUE_WITHMASK_4
+            command_format = SerialPotocolHandler()
+            servo_on_command = command_format.construct_packet(1,command_code, b'\x01\x20\x00\x00\x00\x01\x00\x00\x00\x01', is_response=False)
+            print(f"{command_code.name} Command: ", servo_on_command.hex())
+
+            servo_ctrller.send_command_and_wait_for_response(servo_on_command, f"{command_code.name}", 0.05)
   
             response['message']="Servo turned on successfully."
 
       elif action == "servoOff":
+            command_code = CmdCode.SET_STATE_VALUE_WITHMASK_4
+            command_format = SerialPotocolHandler()
+            servo_off_command = command_format.construct_packet(1,command_code, b'\x01\x20\x00\x00\x00\x00\x00\x00\x00\x01', is_response=False)
+            print(f"{command_code.name} Command: ", servo_off_command.hex())
+
+            servo_ctrller.send_command_and_wait_for_response(servo_off_command, f"{command_code.name}", 0.05)
+
             
             response['message'] = "Servo turned off successfully."
       
