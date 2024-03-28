5374208f9a169a06f5aac27853e0412a6d22b318
JoJoHung <52248840+ChungChiuHung@users.noreply.github.com>
2024-03-28 16:42:26 +0800

    update

--- examples/servo_command_test.py
+++ examples/servo_command_test.py
@@ -27,4 +27,18 @@ print(f"{command_code.name} Response:", get_param_4.hex())
 #=========================================
 command_code = CmdCode.SET_STATE_VALUE_WITHMASK_4
 get_io_input_command = command_format.construct_packet(1, command_code,b'', BitMap.SEL_NO, 3, False)
+print(f"{command_code.name} Command:", get_io_input_command)
 print(f"{command_code.name} Command:", get_io_input_command.hex())
+
+#=========================================
+# SET_PARAM_2 command        
+command_code = CmdCode.SET_PARAM_2
+command_format = SerialPotocolHandler()
+set_param_2_command = command_format.construct_packet(1,command_code, b'\x00\x09\x00\x01', is_response=False)
+print(f"{command_code.name} Command: ", set_param_2_command.hex())
+
+# SERVO_ON Command
+command_code = CmdCode.SET_STATE_VALUE_WITHMASK_4
+command_format = SerialPotocolHandler()
+servo_on_command = command_format.construct_packet(1,command_code, b'\x01\x20\x00\x00\x00\x01\x00\x00\x00\x01', is_response=False)
+print(f"{command_code.name} Command: ", servo_on_command.hex())
