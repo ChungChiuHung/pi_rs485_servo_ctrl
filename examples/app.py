ef73749ae42658b7e31a253676e7bbe2b57875ef
JoJoHung <52248840+ChungChiuHung@users.noreply.github.com>
2024-03-28 16:58:26 +0800

    update

--- examples/app.py
+++ examples/app.py
@@ -185,7 +185,7 @@ def handle_action():
             command_code = CmdCode.SET_STATE_VALUE_WITHMASK_4
             command_format = SerialPotocolHandler()
             set_point_2 = command_format.construct_packet(1, command_code,b'', BitMap.SEL_NO, 2, is_response=False)
-            print(f"{command_code.name} Command:", set_point_1.hex())
+            print(f"{command_code.name} Command:", set_point_2.hex())
 
             servo_ctrller.send_command_and_wait_for_response(set_point_2, f"{command_code.name}", 0.05)
 
