552d8aedce22828d1592741fde52088f8390e42b
ChiuHungChung <NSDrum@hotmail.com>
2024-03-31 19:30:31 +0800

    update

--- servo_communication/__init__.py
+++ servo_communication/__init__.py
@@ -7,3 +7,7 @@ from .servo_command_code import CmdCode
 from .status_bit_mapping import BitMap
 from .servo_serial_protocol_handler import SerialProtocolHandler
 from .servo_control import ServoController
+
+# servo_communication/__init__.py
+
+VERSION = "1.0"
