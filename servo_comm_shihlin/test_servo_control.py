cbd4934e8268eadb58398a857e7544bfa733070c
ChiuHungChung <NSDrum@hotmail.com>
2024-04-11 20:00:12 +0800

    update shihlin

---
+++ servo_comm_shihlin/test_servo_control.py
@@ -0,0 +1,14 @@
+import struct
+from servo_control import ServoController
+from serial_port_manager import SerialPortManager
+from modbus_command_code import CmdCode
+
+if __name__ =="__main__":
+    from serial_port_manager import SerialPortManager
+    serial_manager = SerialPortManager()
+    controller = ServoController(serial_manager)
+    controller.start_motion_sequence([
+        {'code': CmdCode.WRITE_DATA, 'data': struct.pack('>H', 0x1234)},
+        {'code': CmdCode.WRITE_MULTI_DATA, 'data': struct.pack('>H', 0x5678)}
+    ])
+    
