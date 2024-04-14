089c195cc29b423785d0362464b07200cf952cf1
ChiuHungChung <NSDrum@hotmail.com>
2024-04-14 19:41:08 +0800

    update shihlin

--- servo_comm_shihlin/test_serial_port_manager.py
+++ servo_comm_shihlin/test_serial_port_manager.py
@@ -29,11 +29,3 @@ def test_serial_port_manager():
 
 if __name__ == "__main__":
     test_serial_port_manager()
-
-if __name__ == "__main__":
-    serial_manager = SerialPortManager()
-    serial_manager.connect()
-    if serial_manager.get_serial_instance():
-        response = serial_manager.send_and_receive(b'0x01010011')
-        print(response)
-    serial_manager.disconnect()
