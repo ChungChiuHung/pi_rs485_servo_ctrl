ead586c57ff6f17e4e28d616f2557fae32bdd17d
ChiuHungChung <NSDrum@hotmail.com>
2024-04-17 23:56:14 +0800

    update shihlin

--- servo_comm_shihlin/servo_control.py
+++ servo_comm_shihlin/servo_control.py
@@ -75,7 +75,7 @@ class ServoController:
         
     def read_PA01_Ctrl_Mode(self):
         print(f"Address of PD{PA.STY.no} {PA.STY.name}: {PA.STY.address}")
-        message = self.modbus_client.build_read_message(PA.STY.address)
+        message = self.modbus_client.build_read_message(PA.STY.address, 2)
         print(f"Build Read Message: {message}")
         response = self.modbus_client.send_and_receive(message)
         print(f"Respnose Message: {response}")
