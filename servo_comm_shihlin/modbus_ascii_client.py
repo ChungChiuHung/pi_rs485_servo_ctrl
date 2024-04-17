49df3f8e6fe9090942a90b4b90f011fa5b789121
ChiuHungChung <NSDrum@hotmail.com>
2024-04-17 21:53:23 +0800

    update shihlin

--- servo_comm_shihlin/modbus_ascii_client.py
+++ servo_comm_shihlin/modbus_ascii_client.py
@@ -66,6 +66,7 @@ class ModbusASCIIClient:
 
                 if response:
                     print("Response received:", response.decode())
+                    print("Parsing Response", self.parse_read_response(response))
                     return response
                 else:
                     print("No response received.")
@@ -82,6 +83,40 @@ class ModbusASCIIClient:
                 print("Failed to establish serial connection.")
                 return False
         return True
+    
+    def parse_read_response(self, response):
+        if response[0] != ':':
+            raise ValueError("Invalid response: Does not start with ':'")
+        
+        response = response[1:].split('\r\n')[0]
+
+        if len(response) < 11 # Minimal Length with at least on data element
+            raise ValueError("Invalid response: Too short")
+        
+        adr = response[0:2]
+        cmd = response[2:4]
+        data_count = int(response[4:6], 16)
+
+        expected_length = 6 + data_count * 4 + 2
+        if len(response) != expected_length:
+            raise ValueError(f"Invalid response: Length mismatch, expected {expected_length}, got {len(response)}")
+
+        data_hex = response[6:-2]
+        lrc = response[-2:]
+
+        data = [int(data_hex[i:i+4], 16) for i in range(0, len(data_hex), 4)]
+
+        calculated_lrc = self.lrc.calclulate_lrc(response[:-2])
+        if f"{calculated_lrc:02X}" != lrc.upper():
+            raise ValueError("Invalid response: LRC mismatch")
+        
+        return {
+            'Device_number': adr,
+            'Command': cmd,
+            'Data Count': data_count,
+            'Data': data,
+            'LRC':lrc
+        }
     
     def set_di_control_source(self, control_bits):
         data = struct.pack('>H', control_bits)
