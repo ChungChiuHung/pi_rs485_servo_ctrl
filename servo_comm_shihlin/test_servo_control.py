5289e93795be7b4d284a3bacfcf6ca71d42a59f6
ChiuHungChung <NSDrum@hotmail.com>
2024-04-18 20:20:34 +0800

    update shihlin

--- servo_comm_shihlin/test_servo_control.py
+++ servo_comm_shihlin/test_servo_control.py
@@ -51,10 +51,6 @@ if __name__ =="__main__":
     #time.sleep(0.05)
     #print("\n")
 
-    controller.write_0x0206_SON()
-    time.sleep(0.05)
-    print("\n")
-
     controller.read_0x0206_SON()
     time.sleep(0.05)
     print("\n")
