8207068e32d5f12b089a993ca8ecbb09c9d59716
ChiuHungChung <NSDrum@hotmail.com>
2024-04-18 20:46:44 +0800

    update shihlin

--- servo_comm_shihlin/test_servo_control.py
+++ servo_comm_shihlin/test_servo_control.py
@@ -19,9 +19,9 @@ if __name__ =="__main__":
     #time.sleep(0.05)
     #print("\n")
 
-    #controller.write_PD_16_Enable_DI_Control()
-    #time.sleep(0.05)
-    #print("\n")
+    controller.write_PD_16_Enable_DI_Control()
+    time.sleep(0.05)
+    print("\n")
 
     controller.read_PD_16()
     time.sleep(0.05)
