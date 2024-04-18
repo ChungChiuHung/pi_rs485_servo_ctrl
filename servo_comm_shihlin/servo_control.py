f6f4cba501cf5cff544c4a8d52aafccc11ff3a24
ChiuHungChung <NSDrum@hotmail.com>
2024-04-18 21:26:00 +0800

    update shihlin

--- servo_comm_shihlin/servo_control.py
+++ servo_comm_shihlin/servo_control.py
@@ -161,12 +161,12 @@ class ServoController:
 
     def write_PD_01(self):
         print(f"Address of PD{PD.DIA1.no} {PD.DIA1.name}: {PD.DIA1.address}")
-        config_value = ServoUtility.config_hex_with(0, 0, 0, 0)
+        config_value = ServoUtility.config_hex_with(1, 0, 0, 0)
         message = self.modbus_client.build_write_message(PD.DIA1.address, config_value)
         print(f"Build Write Command: {message}")
         response = self.modbus_client.send_and_receive(message)
         print(f"Response Message: {response}")
-        
+
         response_object = ModbusResponse(response)
         print(response_object)
 
