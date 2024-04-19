c1f7fabfc0833939ddfb64efec6cf1ded3b0535e
ChiuHungChung <NSDrum@hotmail.com>
2024-04-20 06:58:03 +0800

    update shihlin

--- servo_comm_shihlin/app.py
+++ servo_comm_shihlin/app.py
@@ -103,7 +103,7 @@ def handle_action():
             time.sleep(0.05)
             servo_ctrller.config_pulses_0x0906_high_byte(0x0780)
             time.sleep(0.05)
-            servo_ctrller.start_continuous_reading(0x0900, 1)
+            servo_ctrller.start_continuous_reading(0x0900, 0.1)
             time.sleep(0.1)
             
             
