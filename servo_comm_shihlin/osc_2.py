7b822e0af224687f1b9b40a726e1ca499a2fe43a
ChiuHungChung <NSDrum@hotmail.com>
2025-01-21 10:53:39 +0800

    update

--- servo_comm_shihlin/osc_2.py
+++ servo_comm_shihlin/osc_2.py
@@ -95,7 +95,7 @@ def set_point_handler(unused_addr, args, angle, acc_time, rpm):
 def reset_initial_abs_position_handler(unused_addr, args):
     try:
         servo_ctrller.write_PA29_Initial_Abs_Pos()
-        send_to_touchdesigner("/reset_initial_abs_position", "reset_initial_abs_position")
+        send_to_touchdesigner("/reset_initial_abs_position", "reset")
         logging.info("Reset initial absolute position.")
     except Exception as e:
         logging.error(f"Error in reset_initial_abs_position_handler: {e}")
